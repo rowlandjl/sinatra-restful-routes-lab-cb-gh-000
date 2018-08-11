2b7d09975e407832f485a3c8fa2b23c746e8ca9c
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-11 01:57:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,7 @@ class ApplicationController < Sinatra::Base
     @recipe = Recipe.find(params[:id])
     @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
 
-    redirect to "/recipes/#{@recipe.id}
+    redirect to "/recipes/#{@recipe.id}"
   end
 
 
