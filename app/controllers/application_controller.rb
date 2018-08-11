2ee4fd61aff89b1200d37962790411e3db3ffd4a
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-11 01:38:35 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,25 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  
+
   get '/' do
     erb :index
   end
 
-  get '/recipes' do 
-    @recipes = Recipe.all 
-    erb :index 
-  end 
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
 
-  get '/recipes/new' do 
-    erb :new 
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do 
+    @recipes= Recipe.new(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    @recipe.save 
+
+    redirect to "/recipes/#{@recipe.id}"
   end 
 
   
